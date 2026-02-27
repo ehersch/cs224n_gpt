6afd24287c32a68599eb58ccb617d1bcff1aeca8
Ethan Hersch <herschethan@gmail.com>
2026-02-27 08:29:44 -0800

    Will add prompts

---
+++ prompts/hallucination_prevention.py
@@ -0,0 +1,3 @@
+"""
+The purpose of this prompt will be to obtain question-answer pairs where the agent does not hallucinate and rather admits it does not know a fact.
+"""
---
+++ prompts/sonnets.py
@@ -0,0 +1,3 @@
+"""
+The purpose of this prompt will be to prompt a larger model (Gemini 3) to produce in-distribition sonnet data.
+"""
